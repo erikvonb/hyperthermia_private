2acc2bbf2bf672ae75785a6fcf8d2cde3ade4ee6
Rebecca Jonasson <rebeccajonasson@Rebeccas-MacBook-Air.local>
2018-06-26 10:11:38 +0200

    Improved convert_temp _matrix so it works with python

--- Treatment_plan/MAIN/Scripts/FEniCS/convert_temp_matrix.m
+++ Treatment_plan/MAIN/Scripts/FEniCS/convert_temp_matrix.m
@@ -1,11 +1,14 @@
-function temp_mat=convert_temp_matrix(temp, tissue_mat)
-
-% Find size
-[a,b,c] = size(tissue_mat);
+function temp_mat=convert_temp_matrix()
 
 current_dir=pwd;
-scriptpath= [current_dir filesep '..' filesep 'Temperature'];
-addpath(scriptpath);
-temp_mat = read_temperature(temp,1,1,1,a,b,c);
+addpath([current_dir filesep '..' filesep 'Temperature' filesep])
+temppath= [current_dir filesep '..' filesep 'FEniCS_results'];
+temp_file=('temperature.h5');
+temp= [temppath filesep temp_file];
+load([temppath filesep 'tissue_mat.mat'])
 
+% Find size
+[a,b,c] = size(tissue_Matrix);
+temp_mat = read_temperature(temp,1,1,1,a,b,c);
+%save('')
 end
