3b7b666fc9504e76f4fad58fa4ad8f90525cb914
Rebecca Jonasson <rebeccajonasson@Rebeccas-MacBook-Air.local>
2018-06-26 10:50:39 +0200

    No input to create_initial_perf_nonlin

--- Treatment_plan/MAIN/Scripts/FEniCS/create_initial_perf_nonlin.m
+++ Treatment_plan/MAIN/Scripts/FEniCS/create_initial_perf_nonlin.m
@@ -1,8 +1,11 @@
-function create_initial_perf_nonlin(tissue_mat, perfusion_mat, modelType)
+function create_initial_perf_nonlin()
 
 % Function that generates a matrix of the perfusion used for calculating
 % the very first temperature in CompletePennes. Here, the intial
 % temperature is assumed to be 37 deg C.
+
+% NOTE: tissue_mat, modelType.txt and perfusion_original.mat needs to be provided
+% in ../Scripts/Input_to_FEniCS 
 
 % Model for modifying the perfusion for muscle, fat and tumor is obtained
 % from "Impact of Nonlinear Heat Transfer on Temperature Control in
@@ -10,11 +13,23 @@ function create_initial_perf_nonlin(tissue_mat, perfusion_mat, modelType)
 
 T=37; %assume initial temp
 
-initial_perf_mat=perfusion_mat;
+% load perfusion (not extrapolated)
+current_dir=pwd;
+datapath=[current_dir filesep '..' filesep 'Input_to_FEniCS' filesep]; 
+load([datapath 'perfusion_original.mat']);
+% load modelType
+fid=fopen([datapath 'modelType.txt'], 'r');
+modelType=fgetl(fid);
+% load tissue mat
+load([datapath 'tissue_mat.mat']);
+tissue_mat=tissue_Matrix;
 
+% Estimate perfusion according to model in report
 perf_muscle= 0.45+3.55*exp(-(T-45)^2/12);
 perf_fat= 0.36+0.36*exp(-(T-45)^2/12);
 perf_tumor= 0.833-(T-37)^4.8/(5.438*10^3);
+
+initial_perf_mat=perfusion;
 
 if startsWith(modelType, 'duke')
     index_muscle = find(tissue_mat==48);
