b20b5d3c5cfe93f829ff4ea502b86c9e38c9d97d
rebjona <rebjona@student.chalmers.se>
2018-07-13 09:33:59 +0200

    commented generate_perf_nonlin

--- Treatment_plan/MAIN/Scripts/FEniCS/generate_perfusion_nonlin.m
+++ Treatment_plan/MAIN/Scripts/FEniCS/generate_perfusion_nonlin.m
@@ -1,18 +1,23 @@
 function perfusion_mat=generate_perfusion_nonlin()
-% ha bas W för första T beräkning, ta in nya T i matlab för att beräkna
-% nytt W och sen använda det för beräkna tredje T, repeat
-% spara W för varje temp-steg så det går läsa in i CompletePennes
-
-% All values and formula for estimating the perfusion are obtained by the
+% Generates a perfusion matrix based on the constant perfusion matrix but the values
+% for fat, muscle and tumor are modified according to a non-linear model. 
+% The model for estimating the perfusion of fat, muscle and tumor is obtained from the
 % report "Impact of Nonlinear Heat Transfer on Temperature Control in
 % Regional Hyperthermia" by Lang, Erdmann, Seebass
 
-% Load data
+% define paths
 current_dir=pwd;
 datapath=[current_dir filesep '..' filesep 'Input_to_FEniCS' filesep];
-% load tissue_mat
+% load tissue mat
+clear ans data
 load([datapath 'tissue_mat.mat']);
-tissue_mat=tissue_Matrix;
+if exist('tissue_Matrix')
+    tissue_mat=tissue_Matrix;
+elseif exist('data')
+    tissue_mat=data;
+elseif exist('ans') 
+    tissue_mat=ans;
+end
 % load modelType
 fid=fopen([datapath 'modelType.txt'], 'r');
 modelType=fgetl(fid);
@@ -26,36 +31,22 @@ else
     disp("Perfusion matrix not loaded correctly");
 end
 
-
-if startsWith(modelType, 'duke') == 1
-    tumor_ind = 80;
-    muscle_ind = 48;
-    cerebellum_ind = 12;
-    water_ind = 81;
-    ext_air_ind = 1;
-    int_air_ind = 2;
-elseif startsWith(modelType,'child') == 1
-    tumor_ind = 9;
-    muscle_ind = 3;
-    cerebellum_ind = 8;
-    water_ind = 30;
-    ext_air_ind = 1;
-    int_air_ind = 5;
-end
-
-    
 % load current temperature
 load([datapath 'temp_current.mat']);
 
+% define indeces for tissues needed
 if startsWith(modelType, 'duke')
     index_muscle = 48;
     index_fat = 27;
     index_tumor = 80;  
-    
+    water_ind = 81;
+    ext_air_ind = 1;
 elseif startsWith(modelType, 'child')
     index_muscle = 3;
     index_fat = 8; % Obs this is WM, cant find fat in tissue_file
     index_tumor = 9;
+    water_ind = 30;
+    ext_air_ind = 1;
 end
 
 % Change perfusion for the tumor -------------------
@@ -70,7 +61,7 @@ perfusion_mat(index_tumor_cold)=0.833;
 temp_leq_42=(temp_mat<=42);
 temp_geq_37=(temp_mat>=37);
 index_tumor_middle= find(temp_leq_42==tumor & temp_geq_37==temp_leq_42 & temp_leq_42==1);
-perfusion_mat(index_tumor_middle)=0.833-(((temp_mat(index_tumor_middle)-37).^4.8)/(5.438*10^3));% assumed 10^3 from report 
+perfusion_mat(index_tumor_middle)=0.833-(((temp_mat(index_tumor_middle)-37).^4.8)/(5.438*10^3)); 
 
 % If temp in tumor > 42C
 temp_higher_42=(temp_mat>42);
@@ -99,23 +90,21 @@ fat=(tissue_mat==index_fat);
 index_fat_less=find(temp_leq_45==fat & temp_leq_45==1);
 perfusion_mat(index_fat_less)=0.36+0.36*exp(-((temp_mat(index_fat_less)-45).^2)/12);
 
-% Is temp in fat > 45C
+% If temp in fat > 45C
 index_fat_hot=find(temp_higher_45==fat & temp_higher_45==1);
 perfusion_mat(index_fat_hot)=0.72;
 
 %-----------------------------------------------------
 
+% save
 perf_mat=perfusion_mat;
 addpath([current_dir filesep '..' filesep] )
 save(get_path('perfusion_mat'), 'perf_mat', '-v7.3');
 
+% extrapolate to a form suitable to the python code and save 
 interior_mat = tissue_mat ~= water_ind & tissue_mat ~= ext_air_ind;
 [~,nearest_points] = Extrapolation.meijster(interior_mat);
 finalize('perfusion_mat', nearest_points, modelType);
-
-%savepath=  [datapath 'perfusion_current'];
-%save(savepath, 'perf_mat', '-v7.3');
-% borde man spara in m olika index så man kan kolla på dom senare?
 
 end
 
